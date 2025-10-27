540dadc79f7843047de5e068df53490ee43bb0b9
lixin <lixin@windimg.com>
2025-10-27 16:15:33 +0800

    sec section

--- src/components/sec-section/index.tsx
+++ src/components/sec-section/index.tsx
@@ -1,29 +1,37 @@
-import nextBg from '@/assets/next-gen.svg';
-import styles from './index.module.less';
-import { isPC } from '@/utils/platform';
-import cx from 'classnames';
-import gupiao from '@/assets/gupiao.svg';
-import quanqiu from '@/assets/quanqiu.svg';
-import jiaoyi from '@/assets/jiaoyi.svg';
+import gupiao from "@/assets/gupiao.svg"
+import jiaoyi from "@/assets/jiaoyi.svg"
+import nextBg from "@/assets/next-gen.svg"
+import quanqiu from "@/assets/quanqiu.svg"
+import { isPC } from "@/utils/platform"
+import cx from "classnames"
+import styles from "./index.module.less"
 
 export default function SecSection() {
-    return <section className={cx(styles.secSection, {
+  return (
+    <section
+      className={cx(styles.secSection, {
         [styles.h5]: !isPC,
-    })}>
-        <img src={nextBg} alt="nextBg" className={styles.nextBg} />
-        <div className={styles.content}>
-            <div className={styles.item}>
-                <img src={quanqiu} className={styles.itemImg} />
-                <span className={styles.itemTitle}>Barrier-Free Global Stock Trading</span>
-            </div>
-            <div className={styles.item}>
-                <img src={jiaoyi} className={styles.itemImg} />
-                <span className={styles.itemTitle}>24/7 Trading</span>
-            </div>
-            <div className={styles.item}>
-                <img src={gupiao} className={styles.itemImg} />
-                <span className={styles.itemTitle}>50x Leverage</span>
-            </div>
+      })}
+    >
+      <img src={nextBg} alt="nextBg" className={styles.nextBg} />
+      <div className={styles.content}>
+        <div className={styles.item}>
+          <img src={quanqiu} className={styles.itemImg} />
+          <span className={styles.itemTitle}>Barrier-Free Global Stock Trading</span>
         </div>
-    </section>;
+        <div className={styles.item}>
+          <img src={jiaoyi} className={styles.itemImg} />
+          <span className={styles.itemTitle}>24/7 Trading</span>
+        </div>
+        <div className={styles.item}>
+          <img src={gupiao} className={styles.itemImg} />
+          <span className={styles.itemTitle}>
+            2000+ Stocks
+            <br />
+            Truly decentralized, No Pre-Deposit
+          </span>
+        </div>
+      </div>
+    </section>
+  )
 }
