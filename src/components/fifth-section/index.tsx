29a41d5611001ae8fcafd5f29b1a58ed3796719c
lixin <lixin@windimg.com>
2025-10-27 17:09:15 +0800

    fifth section

---
+++ src/components/fifth-section/index.tsx
@@ -0,0 +1,48 @@
+import investorsAndPartners from "@/assets/investors-and-partners.svg"
+import Binance from "@/assets/investors/Binance.svg"
+import Bingx from "@/assets/investors/bingx.svg"
+import Dgcap from "@/assets/investors/DGCapital.svg"
+import Gate from "@/assets/investors/Gate.svg"
+import IBKR from "@/assets/investors/IBKR.svg"
+import Iobc from "@/assets/investors/IOBC.svg"
+import Itiger from "@/assets/investors/Itiger.svg"
+import Okx from "@/assets/investors/OKX.svg"
+import { isPC } from "@/utils/platform"
+import cx from "classnames"
+import styles from "./index.module.less"
+
+const logos = [
+  { src: Binance, url: "https://x.com/BinanceWallet" },
+  { src: Iobc, url: "https://x.com/iobc_capital" },
+  { src: Bingx, url: "https://x.com/BingXOfficial" },
+  { src: Dgcap, url: "https://x.com/DG__Capital" },
+  { src: Okx, url: "https://x.com/wallet" },
+  { src: Gate, url: "https://x.com/GateWeb3_HQ" },
+  { src: IBKR, url: "https://x.com/IBKR" },
+  { src: Itiger, url: "https://www.itiger.com/hk/" },
+]
+
+export default function FifthSection() {
+  return (
+    <section
+      className={cx(styles.fifthSection, {
+        [styles.h5]: !isPC,
+      })}
+    >
+      <img src={investorsAndPartners} className={styles.investorsAndPartners} />
+
+      {/* 横向滚动容器 */}
+      <div className={styles.scrollWrapper}>
+        <div className={styles.scrollTrack}>
+          {[...logos, ...logos].map((item, idx) => (
+            <div className={styles.logoItem} key={idx}>
+              <a href={item.url} target="_blank">
+                <img className={styles.logoImg} src={item.src} />
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,27 @@
-import styles from './index.module.less';
-import cx from 'classnames';
-import { isPC } from '@/utils/platform';
-import FirstSection from '@/components/first-section';
-import { lazy } from 'react';
-const SecSection = lazy(() => import('@/components/sec-section'));
-const ThirdSection = lazy(() => import('@/components/third-section'));
-const ForthSection = lazy(() => import('@/components/forth-section'));
-const Footer = lazy(() => import('@/components/footer'));
+import FirstSection from "@/components/first-section"
+import { isPC } from "@/utils/platform"
+import cx from "classnames"
+import { lazy } from "react"
+import styles from "./index.module.less"
+const SecSection = lazy(() => import("@/components/sec-section"))
+const ThirdSection = lazy(() => import("@/components/third-section"))
+const ForthSection = lazy(() => import("@/components/forth-section"))
+const FifthSection = lazy(() => import("@/components/fifth-section"))
+const Footer = lazy(() => import("@/components/footer"))
 
 export default function Home() {
-  return <div className={cx(styles.wrapper, {
-    [styles.h5]: !isPC,
-  })}>
-    <FirstSection />
-    <SecSection />
-    <ThirdSection />
-    <ForthSection />
-    <Footer />
-  </div>;
+  return (
+    <div
+      className={cx(styles.wrapper, {
+        [styles.h5]: !isPC,
+      })}
+    >
+      <FirstSection />
+      <SecSection />
+      <ThirdSection />
+      <ForthSection />
+      <FifthSection />
+      <Footer />
+    </div>
+  )
 }
