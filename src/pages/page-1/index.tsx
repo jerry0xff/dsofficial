95fcea6f337aaa9a97eac4eab85020d896fc7a63
lixin <lixin@windimg.com>
2026-01-06 17:18:49 +0800

    map

--- src/pages/page-1/index.tsx
+++ src/pages/page-1/index.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react"
 import PartnersRow from "./PartnersRow"
 import StatsRow from "./StatsRow"
 import TickerStrip from "./TickerStrip"
 
 export default function PageOne() {
+  const backgrounds = [
+    "/assets/page-1/map-empty.svg",
+    "/assets/page-1/map-nyc.svg",
+    "/assets/page-1/map-sf.svg",
+    "/assets/page-1/map-sh.svg",
+    "/assets/page-1/map-tokyo.svg",
+  ]
+  const [backgroundIndex, setBackgroundIndex] = useState(0)
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setBackgroundIndex((current) => (current + 1) % backgrounds.length)
+    }, 5000)
+
+    return () => {
+      window.clearInterval(intervalId)
+    }
+  }, [backgrounds.length])
+
   return (
     <section
-      className="flex min-h-[calc(100vh-60px)] flex-col items-center px-4 pt-2 bg-[url('/assets/page-1/map-empty.svg')] bg-contain bg-no-repeat"
-      style={{ backgroundPosition: "center 70%" }}
+      className="flex min-h-[calc(100vh-60px)] flex-col items-center px-4 pt-2 bg-contain bg-no-repeat"
+      style={{
+        backgroundImage: `url('${backgrounds[backgroundIndex]}')`,
+        backgroundPosition: "center 70%",
+      }}
     >
       <TickerStrip />
       <div className="mt-[73px] text-center text-[48px] font-black uppercase text-white font-['DM_Sans',system-ui,sans-serif]">
