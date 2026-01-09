2ec24c7e100d3baf2eeb3087da695a8001c4a6e4
lixin <lixin@windimg.com>
2026-01-09 13:56:14 +0800

    pc header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
 
   return (
     <header className="fixed left-0 top-0 z-50 h-[60px] w-full bg-[#0A0A17] text-white">
-      <div className="mx-auto flex h-full items-center justify-between px-6">
+      <div className="mx-auto flex h-full items-center justify-between px-5">
         <div className="flex items-center gap-8">
           <div className="flex items-center gap-3">
             <img src="/assets/logo.svg" alt="DeShare" className="h-6 w-[129px]" />
