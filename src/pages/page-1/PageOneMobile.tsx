38c0e2d6b0bf1e81cb65e915f2eda205ac4be270
lixin <lixin@windimg.com>
2026-01-09 15:21:08 +0800

    map

--- src/pages/page-1/PageOneMobile.tsx
+++ src/pages/page-1/PageOneMobile.tsx
@@ -8,7 +8,7 @@ export default function PageOneMobile({ className = "" }: PageOneMobileProps) {
   return (
     <section
       className={[
-        "relative flex min-h-[calc(100vh-60px)] flex-col items-center px-4 pt-2 overflow-hidden bg-[#0A0A17]",
+        "relative flex min-h-[calc(100vh-60px)] flex-col items-center px-4 pt-6 overflow-hidden bg-[#0A0A17]",
         className,
       ].join(" ")}
     >
@@ -23,6 +23,11 @@ export default function PageOneMobile({ className = "" }: PageOneMobileProps) {
         changeSize={10}
         changeIconSize={10}
       />
+      <div className="mt-[90px] w-[100vw] -mx-4 overflow-hidden flex justify-center">
+        <div className="h-[348px] w-[670px] flex-none">
+          <img src="/assets/page-1/map-empty.svg" alt="" className="h-full w-full" />
+        </div>
+      </div>
     </section>
   )
 }
