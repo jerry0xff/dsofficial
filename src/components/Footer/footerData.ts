525d4c3975fde659e8c2cdcb72df6849cbd1d88e
lx <lixinit@gmail.com>
2026-01-10 22:40:08 +0800

    footer

---
+++ src/components/Footer/footerData.ts
@@ -0,0 +1,14 @@
+export const currentYear = new Date().getFullYear()
+
+export const footerTagline = ["Borderless, Barrier-Free", "Invest in Global Stocks On-Chain"]
+
+export const footerSections = [
+  {
+    title: "Documentation",
+    links: ["Whitepaper"],
+  },
+  {
+    title: "Community",
+    links: ["Telegram", "X (Twitter)"],
+  },
+]
