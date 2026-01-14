85a817273c7038aea575fd27ea533728ce60ef9b
lixin <lixin@windimg.com>
2026-01-14 16:42:01 +0800

    bugfix

--- src/pages/page-4/PopularTable.tsx
+++ src/pages/page-4/PopularTable.tsx
@@ -99,7 +99,7 @@ export default function PopularTable({
         {title}
       </div>
       <div className="flex-1 overflow-y-auto px-0 md:px-4 no-scrollbar mt-0">
-        <div className="text-[10px] md:text-[12px] font-medium leading-none geist-mono tracking-[0.02em] text-white/80 shadow-[0px_1px_0px_0px_rgba(0,0,0,0.25)]">
+        <div className="text-[10px] md:text-[12px] font-medium leading-none geist-mono tracking-[0.02em] text-white/80">
           {rows.map((row, index) => {
             const isUp = row.change.startsWith("+")
             const trendColor = isUp ? "var(--ColorCyanDefault)" : "var(--ColorDownDefault)"
